817e4e4fe50dd18c28134e6e963686fbcd04930d
krtong <kevin.r.tong@gmail.com>
2025-02-28 18:08:16 -0800

    add .gitignore

--- __tests__/messaging.test.js
+++ __tests__/messaging.test.js
@@ -14,7 +14,19 @@ describe('Chrome Extension Message Handling', () => {
   let messageListener;
   
   beforeEach(() => {
-    // Capture the message listener function
+    // Set up the Chrome mock
+    global.chrome = {
+      runtime: {
+        onMessage: {
+          addListener: jest.fn()
+        }
+      }
+    };
+    
+    // Import content script to register the listener
+    require('../web_extension/chrome/content.js');
+    
+    // Now the listener should be registered
     messageListener = chrome.runtime.onMessage.addListener.mock.calls[0][0];
     mockSendResponse = jest.fn();
     
